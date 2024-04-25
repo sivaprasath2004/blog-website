23241d89c276c2cb1f7b92026394b76f9ea46045
sivaprasath2004 <prasathsiva2004@gmail.com>
2024-04-25 07:23:05 +0530

    blog

--- src/Blog.js
+++ src/Blog.js
@@ -1,8 +1,17 @@
 import React, { useState, useRef } from "react";
+import axios from "axios";
 import "./Blog.css";
 const Blog = () => {
+  const [checker, setChecker] = useState({});
   const [image, setImage] = useState(null);
   const fileInputRef = useRef(null);
+  const handleTitle = (e) =>
+    setChecker((pre) => ({ ...pre, title: e.target.value }));
+  const handleDescrption = (e) =>
+    setChecker((pre) => ({ ...pre, Des: e.target.value }));
+  const handleTags = (e) =>
+    setChecker((pre) => ({ ...pre, Tags: e.target.value }));
+  const handleCancel = () => setImage(null);
   const handleDrop = (e) => {
     e.preventDefault();
     const file = e.dataTransfer.files[0];
@@ -14,7 +23,6 @@ const Blog = () => {
       reader.readAsDataURL(file);
     }
   };
-
   const handleDragOver = (e) => {
     e.preventDefault();
   };
@@ -29,14 +37,26 @@ const Blog = () => {
       reader.readAsDataURL(file);
     }
   };
-
   const handleUploadButtonClick = () => {
     fileInputRef.current.click();
   };
+  async function handleUploadToBlog() {
+    if (image && checker.title && checker.Des) {
+      let res = await axios.post("http://localhost:5000/Blog", {
+        Image: image,
+        title: checker.title,
+        Des: checker.Des,
+        Tags: checker.Tags,
+      });
+      console.log(res);
+    } else {
+      console.log(false);
+    }
+  }
   return (
     <section id="Blog">
       <div className="input_container" id="Title">
-        <input type="text" placeholder="Title" />
+        <input type="text" placeholder="Title" onChange={handleTitle} />
       </div>
       <div
         className="Drag_Drop"
@@ -52,7 +72,7 @@ const Blog = () => {
             />
             <button
               className="cancel_image_button"
-              onClick={() => console.log("Hello")}
+              onClick={() => handleCancel()}
             >
               cancel
             </button>
@@ -70,10 +90,17 @@ const Blog = () => {
           ref={fileInputRef}
         />
       </div>
-      <textarea placeholder="Description" style={{ resize: "vertical" }} />
+      <textarea
+        placeholder="Description"
+        style={{ resize: "vertical" }}
+        onChange={handleDescrption}
+      />
       <div className="input_container" id="Tags">
-        <input type="text" placeholder="Tags" />
+        <input type="text" placeholder="Tags" onChange={handleTags} />
       </div>
+      <button id="upload" onClick={handleUploadToBlog}>
+        Upload
+      </button>
     </section>
   );
 };
